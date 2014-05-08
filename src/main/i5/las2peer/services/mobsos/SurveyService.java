615e45edd25eb011d4372f64bc1e3a33da56d45f
Dominik Renzel <dominik.renzel@gmail.com>
2014-05-08 15:06:11 +0200

    introduced method for updating surveys; added more testcases; parsing of survey data passed in request body now with validation

--- src/main/i5/las2peer/services/mobsos/SurveyService.java
+++ src/main/i5/las2peer/services/mobsos/SurveyService.java
@@ -6,14 +6,14 @@ All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright notice, this
+ * Redistributions of source code must retain the above copyright notice, this
   list of conditions and the following disclaimer.
 
-* Redistributions in binary form must reproduce the above copyright notice,
+ * Redistributions in binary form must reproduce the above copyright notice,
   this list of conditions and the following disclaimer in the documentation
   and/or other materials provided with the distribution.
 
-* Neither the name of the {organization} nor the names of its
+ * Neither the name of the {organization} nor the names of its
   contributors may be used to endorse or promote products derived from
   this software without specific prior written permission.
 
@@ -27,24 +27,25 @@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 package i5.las2peer.services.mobsos;
 
 import i5.las2peer.api.Service;
-import i5.las2peer.p2p.NodeInformation;
 import i5.las2peer.restMapper.HttpResponse;
 import i5.las2peer.restMapper.RESTMapper;
 import i5.las2peer.restMapper.annotations.ContentParam;
 import i5.las2peer.restMapper.annotations.DELETE;
 import i5.las2peer.restMapper.annotations.GET;
 import i5.las2peer.restMapper.annotations.POST;
+import i5.las2peer.restMapper.annotations.PUT;
 import i5.las2peer.restMapper.annotations.Path;
 import i5.las2peer.restMapper.annotations.PathParam;
 import i5.las2peer.restMapper.annotations.Version;
 import i5.las2peer.security.UserAgent;
-import i5.las2peer.tools.CryptoException;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -54,6 +55,7 @@ import java.sql.Statement;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.NoSuchElementException;
 import java.util.TimeZone;
@@ -70,18 +72,18 @@ import org.json.simple.parser.ParseException;
  * 
  * MobSOS Survey Service
  * 
+ * TODO: add class documentation
+ * 
  * @author Dominik Renzel
  *
  */
-//@Path("mobsos")
+@Path("mobsos")
 @Version("0.1")
-//@Consumes("application/json")
-//@Produces("application/json")
 public class SurveyService extends Service {
 
 	private Connection connection;
 	private PreparedStatement surveyInsertStatement, surveysQueryStatement, surveysDeleteStatement;
-	private PreparedStatement surveyQueryStatement, surveyDeleteStatement;
+	private PreparedStatement surveyQueryStatement, surveyCheckOwnerStatement, surveyDeleteStatement, surveyUpdateStatement;
 
 	private String epUrl;
 	private String jdbcDriverClassName;
@@ -107,10 +109,10 @@ public class SurveyService extends Service {
 	}
 
 	@GET
-	@Path("mobsos/surveys")
+	@Path("surveys")
 	public HttpResponse getSurveys()
 	{
-		
+
 		try {
 			JSONObject r = new JSONObject();
 			JSONArray a = new JSONArray();
@@ -143,11 +145,24 @@ public class SurveyService extends Service {
 	 * 
 	 */
 	@POST
-	@Path("mobsos/surveys")
+	@Path("surveys")
 	public HttpResponse createSurvey(@ContentParam String content)
 	{
 		try {
-			int sid = storeSurvey(content);
+			JSONObject o;
+			
+			try{
+				o = parseSurvey(content);
+			} catch (IllegalArgumentException | ParseException e){
+				HttpResponse result = new HttpResponse(e.getMessage());
+				result.setStatus(400);
+				return result;
+			}
+			
+			int sid = storeNewSurvey(o);
+			
+			
+			
 			JSONObject r = new JSONObject();
 			r.put("url",epUrl + "mobsos/surveys/" + sid);
 			HttpResponse result = new HttpResponse(r.toJSONString());
@@ -164,10 +179,11 @@ public class SurveyService extends Service {
 	}
 
 	/**
-	 * Deletes all surveys at once. This method should be used with caution!
+	 * Deletes all surveys at once without any check for ownership. 
+	 * This method should be only be used for development and with absolute caution!
 	 */
 	@DELETE
-	@Path("mobsos/surveys")
+	@Path("surveys")
 	public HttpResponse deleteSurveys(){
 		try {
 			surveysDeleteStatement.executeUpdate();
@@ -191,7 +207,7 @@ public class SurveyService extends Service {
 	 * @return
 	 */
 	@GET
-	@Path("mobsos/surveys/{id}")
+	@Path("surveys/{id}")
 	public HttpResponse getSurvey(@PathParam("id") int id){
 
 		try {
@@ -200,13 +216,13 @@ public class SurveyService extends Service {
 
 			ResultSet rs = surveyQueryStatement.executeQuery();
 
-			if (rs == null){
+			if (!rs.isBeforeFirst()){
 				HttpResponse result = new HttpResponse("Survey " + id + " does not exist!");
 				result.setStatus(404);
 				return result;
 			}
 
-			JSONObject r = parseSurvey(rs);
+			JSONObject r = readSurveyFromResultSet(rs);
 			HttpResponse result = new HttpResponse(r.toJSONString());
 			result.setStatus(200);
 			return result;
@@ -217,20 +233,105 @@ public class SurveyService extends Service {
 			return result;
 		}
 	}
-	
-	@DELETE
-	@Path("mobsos/surveys/{id}")
-	public HttpResponse deleteSurvey(@PathParam("id") int id){
+
+	/**
+	 * Updates a survey with a given id. The respective survey may only be deleted, if the active agent is the survey's owner.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@POST
+	@Path("surveys/{id}")
+	public HttpResponse updateSurvey(@PathParam("id") int id, @ContentParam String content){
+
 		try {
+			int exown;
+			exown = checkSurveyExistenceOwnership(id);
+
+			// check if survey exists; if not, return 404.
+			if(exown == -1){
+				HttpResponse result = new HttpResponse("Survey " + id + " does not exist.");
+				result.setStatus(404);
+				return result;
+			} 
+			// if survey exists, check if active agent is owner. if not, return 401.
+			else if (exown == 0){
+				HttpResponse result = new HttpResponse("Survey " + id + " may only be deleted by its owner.");
+				result.setStatus(401);
+				return result;
+			}
+
+			// if survey exists and active agent is owner, proceed.
+			
+			JSONObject o;
+			// parse and validate content. If invalid, return 400 (bad request)
+			try{
+				o = parseSurvey(content);
+			} catch (IllegalArgumentException | ParseException e){
+				HttpResponse result = new HttpResponse(e.getMessage());
+				result.setStatus(400);
+				return result;
+			}
+			surveyUpdateStatement.clearParameters();
+			surveyUpdateStatement.setString(1, (String) o.get("name") );
+			surveyUpdateStatement.setString(2, (String) o.get("description") );
+			surveyUpdateStatement.setString(3, (String) o.get("resource") );
+			surveyUpdateStatement.setTimestamp(4, new Timestamp(DatatypeConverter.parseDateTime((String)o.get("start")).getTimeInMillis()));
+			surveyUpdateStatement.setTimestamp(5, new Timestamp(DatatypeConverter.parseDateTime((String)o.get("end")).getTimeInMillis()));
+			surveyUpdateStatement.setInt(6, id);
+
+			surveyUpdateStatement.executeUpdate();
+			connection.commit();
+
+			HttpResponse result = new HttpResponse("Survey " + id + " updated.");
+			result.setStatus(200);
+			return result;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			HttpResponse result = new HttpResponse("Internal Error: " + e.getMessage());
+			result.setStatus(500);
+			return result;
+		}
+	}
+
+	/**
+	 * Deletes a survey with a given id. The respective survey may only be deleted, if the active agent is the survey's owner.
+	 * 
+	 * @param id
+	 */
+	@DELETE
+	@Path("surveys/{id}")
+	public HttpResponse deleteSurvey(@PathParam("id") int id){
+
+		try {
+
+			int exown = checkSurveyExistenceOwnership(id);
+
+			// check if survey exists; if not, return 404.
+			if(exown == -1){
+				HttpResponse result = new HttpResponse("Survey " + id + " does not exist.");
+				result.setStatus(404);
+				return result;
+			} 
+			// if survey exists, check if active agent is owner. if not, return 401.
+			else if (exown == 0){
+				HttpResponse result = new HttpResponse("Survey " + id + " may only be deleted by its owner.");
+				result.setStatus(401);
+				return result;
+			}
+
 			surveyDeleteStatement.clearParameters();
 			surveyDeleteStatement.setInt(1, id);
-			
+
 			int r = surveyDeleteStatement.executeUpdate();
-			System.out.println("Deletion result: " + r);
 			connection.commit();
-		
-			HttpResponse result = new HttpResponse("");
+
+			HttpResponse result;
+
+			result = new HttpResponse("Survey " + id + " deleted.");
 			result.setStatus(200);
+
 			return result;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -240,8 +341,39 @@ public class SurveyService extends Service {
 		}
 	}
 
-	@SuppressWarnings("unchecked")
-	private JSONObject parseSurvey(ResultSet rs) throws SQLException{
+	// ---------------------------- private helper methods -----------------------
+
+	/**
+	 * Checks if survey exists and active agent is owner.
+	 * 
+	 * @param surveyId
+	 * @return -1 if survey does not exist, 0 if active agent is not owner, 1 if active agent is owner
+	 * @throws SQLException 
+	 */
+	private int checkSurveyExistenceOwnership(int surveyId) throws SQLException{
+		surveyCheckOwnerStatement.clearParameters();
+		surveyCheckOwnerStatement.setInt(1, surveyId);
+
+		ResultSet rs = surveyCheckOwnerStatement.executeQuery();
+
+		if (!rs.isBeforeFirst()){
+			return -1; // survey does not exist
+		}
+
+		rs.next();
+
+		String owner = rs.getString(1);
+		System.out.println("Survey Owner: " + owner + " Active Agent: " + this.getActiveAgent().getId());
+
+		if(!owner.equals(""+this.getActiveAgent().getId())){
+			return 0;
+		} else {
+			return 1;
+		}
+
+	}
+
+	private JSONObject readSurveyFromResultSet(ResultSet rs) throws SQLException{
 		if(rs == null){
 			return null; 
 		}
@@ -276,11 +408,97 @@ public class SurveyService extends Service {
 			return null;
 		}
 	}
-
 	/**
-	 * @throws ClassNotFoundException
-	 * @throws SQLException
+	 * Parses incoming content to a JSON Object including checks for completeness, illegal fields and values.
+	 * 
+	 * @param content
+	 * @return
+	 * @throws ParseException
+	 * @throws IllegalArgumentException
 	 */
+	private JSONObject parseSurvey(String content) throws ParseException, IllegalArgumentException {
+		JSONObject o = (JSONObject) JSONValue.parseWithException(content);
+
+		// check result for unknown illegal fields. If so, parsing fails.
+		String[] fields = {"id","owner","name","description","resource","start","end"};
+		for (Object key: o.keySet()){
+			if(!Arrays.asList(fields).contains(key)){
+
+				throw new IllegalArgumentException("Illegal survey field '" + key + "' detected!");
+
+			} else {
+				if(key.equals("name") && !(o.get(key) instanceof String)){
+					throw new IllegalArgumentException("Illegal value for survey field 'name'. Should be a string.");
+				}
+				else if(key.equals("description") && !(o.get(key) instanceof String)){
+					throw new IllegalArgumentException("Illegal value for survey field 'description'. Should be a string.");
+				}
+				else if(key.equals("resource")){
+					try {
+						new URL((String) o.get(key));
+					} catch (MalformedURLException e) {
+						throw new IllegalArgumentException("Illegal value for survey field 'resource'. Should be a valid URL.");
+					}
+				} 
+				else if(key.equals("start")){
+					try{
+						DatatypeConverter.parseDateTime((String)o.get("start"));
+					} catch (IllegalArgumentException e){
+						throw new IllegalArgumentException("Illegal value for survey field 'start'. Should be an ISO-8601 formatted time string.");
+					}
+				}
+				else if(key.equals("end")){
+					try{
+						DatatypeConverter.parseDateTime((String)o.get("end"));
+					} catch (IllegalArgumentException e){
+						throw new IllegalArgumentException("Illegal value for survey field 'start'. Should be an ISO-8601 formatted time string.");
+					}
+				}
+			}
+		}
+
+		// check if all necessary fields are specified.
+		if(	o.get("name") == null || 
+				o.get("description") == null || 
+				o.get("resource") == null || 
+				o.get("start") == null ||
+				o.get("end") == null){
+			throw new IllegalArgumentException("Survey data incomplete! All fields name, description, resource, start, and end must be defined!");
+		}
+		
+		// finally check time integrity constraint: start must be before end (possibly not enforced by database; mySQL does not support this check)
+		long d_start = DatatypeConverter.parseDateTime((String)o.get("start")).getTimeInMillis();
+		long d_end = DatatypeConverter.parseDateTime((String)o.get("end")).getTimeInMillis();
+		
+		if(d_start >= d_end){
+			throw new IllegalArgumentException("Survey data invalid! Start time must be before end time!");
+		}
+		
+		return o;
+	}
+
+	private int storeNewSurvey(JSONObject o) throws IllegalArgumentException, SQLException{
+
+			surveyInsertStatement.clearParameters();
+			surveyInsertStatement.setString(1, ""+this.getActiveAgent().getId()); // active agent becomes owner automatically
+			surveyInsertStatement.setString(2, (String) o.get("name"));
+			surveyInsertStatement.setString(3, (String) o.get("description"));
+			surveyInsertStatement.setString(4, (String) o.get("resource"));
+			surveyInsertStatement.setTimestamp(5, new Timestamp(DatatypeConverter.parseDateTime((String)o.get("start")).getTimeInMillis()));
+			surveyInsertStatement.setTimestamp(6, new Timestamp(DatatypeConverter.parseDateTime((String)o.get("end")).getTimeInMillis()));
+
+			surveyInsertStatement.executeUpdate();
+			ResultSet rs = surveyInsertStatement.getGeneratedKeys();
+
+			connection.commit();
+
+			if (rs.next()) {
+				return rs.getInt(1);
+			} else {
+				throw new NoSuchElementException("No new id was created");
+			}
+	}
+
 	private void initDatabaseConnection() throws ClassNotFoundException, SQLException{
 
 		Class.forName(jdbcDriverClassName);
@@ -290,8 +508,10 @@ public class SurveyService extends Service {
 		surveyInsertStatement = connection.prepareStatement("insert into " + jdbcSchema + ".survey(owner, name, description, resource, start, end ) values (?,?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
 		surveysQueryStatement = connection.prepareStatement("select id from " + jdbcSchema + ".survey");
 		surveysDeleteStatement = connection.prepareStatement("delete from "+ jdbcSchema + ".survey");
-		
+
 		surveyQueryStatement = connection.prepareStatement("select * from " + jdbcSchema + ".survey where id = ?");
+		surveyCheckOwnerStatement = connection.prepareStatement("select owner from " + jdbcSchema + ".survey where id = ?");
+		surveyUpdateStatement = connection.prepareStatement("update "+ jdbcSchema + ".survey set name=?, description=?, resource=?, start=?, end=? where id = ?");
 		surveyDeleteStatement = connection.prepareStatement("delete from "+ jdbcSchema + ".survey where id = ?");
 
 	}
@@ -312,59 +532,6 @@ public class SurveyService extends Service {
 			e.printStackTrace();
 		}
 		return result;
-	}
-
-	private int storeSurvey(String content) throws IllegalArgumentException, SQLException{
-
-		JSONObject o;
-
-		try {
-			o = (JSONObject) JSONValue.parseWithException(content);
-
-			if(	o.get("name") == null || 
-					o.get("description") == null || 
-					o.get("resource") == null || 
-					o.get("start") == null ||
-					o.get("end") == null){
-				throw new IllegalArgumentException("Invalid data format detected! Each survey data object must have fields.");
-			}
-
-			// maybe in future we are able to have OpenID Connect identities here...
-			String owner;
-			if(this.getActiveAgent() instanceof UserAgent){
-				UserAgent u = (UserAgent) this.getActiveAgent();
-				if(u.getEmail() != null){
-					owner = u.getEmail();
-				} else {
-					owner = ""+u.getId();
-				}
-			} else {
-				owner = "" + this.getActiveAgent().getId();
-			}
-
-			surveyInsertStatement.clearParameters();
-			surveyInsertStatement.setString(1, ""+this.getActiveAgent().getId());
-			surveyInsertStatement.setString(2, (String) o.get("name"));
-			surveyInsertStatement.setString(3, (String) o.get("description"));
-			surveyInsertStatement.setString(4, (String) o.get("resource"));
-			surveyInsertStatement.setTimestamp(5, new Timestamp(DatatypeConverter.parseDateTime((String)o.get("start")).getTimeInMillis()));
-			surveyInsertStatement.setTimestamp(6, new Timestamp(DatatypeConverter.parseDateTime((String)o.get("end")).getTimeInMillis()));
-
-			surveyInsertStatement.executeUpdate();
-			ResultSet rs = surveyInsertStatement.getGeneratedKeys();
-
-			connection.commit();
-
-			if (rs.next()) {
-				return rs.getInt(1);
-			} else {
-				throw new NoSuchElementException("No new id was created");
-			}
-
-		} catch (ParseException e) {
-			throw new IllegalArgumentException("Invalid data format detected! Data must be JSON-formatted. " + e.getMessage());	
-		}
-
 	}
 
 }
--- src/test/i5/las2peer/services/mobsos/SurveyServiceTest.java
+++ src/test/i5/las2peer/services/mobsos/SurveyServiceTest.java
@@ -6,14 +6,14 @@ All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright notice, this
+ * Redistributions of source code must retain the above copyright notice, this
   list of conditions and the following disclaimer.
 
-* Redistributions in binary form must reproduce the above copyright notice,
+ * Redistributions in binary form must reproduce the above copyright notice,
   this list of conditions and the following disclaimer in the documentation
   and/or other materials provided with the distribution.
 
-* Neither the name of the {organization} nor the names of its
+ * Neither the name of the {organization} nor the names of its
   contributors may be used to endorse or promote products derived from
   this software without specific prior written permission.
 
@@ -31,10 +31,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package i5.las2peer.services.mobsos;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import i5.las2peer.p2p.LocalNode;
+import i5.las2peer.restMapper.MediaType;
+import i5.las2peer.restMapper.data.Pair;
 import i5.las2peer.security.ServiceAgent;
 import i5.las2peer.security.UserAgent;
 import i5.las2peer.testing.MockAgentFactory;
@@ -167,6 +167,7 @@ public class SurveyServiceTest {
 	public void testSurveyCreation()
 	{
 		try {
+
 			ClientResponse result=c.sendRequest("POST", "mobsos/surveys",generateSurveyJSON().toJSONString());
 
 			assertEquals(201, result.getHttpCode());
@@ -185,6 +186,29 @@ public class SurveyServiceTest {
 			e.printStackTrace();
 			fail("Service should return a valid URL to the new survey's resource");
 		}
+	}
+
+	@Test
+	public void testBadRequestSurveyCreation()
+	{
+		JSONObject invalidSurvey = generateSurveyJSON(); // until now, survey JSON is ok. Introduce problems now...
+		invalidSurvey.put("name", new Integer(2)); // name must be string
+		
+		ClientResponse result=c.sendRequest("POST", "mobsos/surveys",invalidSurvey.toJSONString());
+		assertEquals(400, result.getHttpCode());
+		
+		invalidSurvey.put("name", "Alcoholics Standard Survey"); //make valid again and introduce other problem
+		invalidSurvey.put("start", "20144-33-44T1000"); //introduce wrong time
+		
+		result=c.sendRequest("POST", "mobsos/surveys",invalidSurvey.toJSONString());
+		assertEquals(400, result.getHttpCode());
+		
+		invalidSurvey.put("start", "2014-05-08T12:00:00Z"); //make valid again and introduce other problem
+		invalidSurvey.put("end", "2014-04-01T00:00:00Z"); // end time before start time
+		
+		result=c.sendRequest("POST", "mobsos/surveys",invalidSurvey.toJSONString());
+		assertEquals(400, result.getHttpCode());
+		
 	}
 
 	@Test
@@ -251,7 +275,7 @@ public class SurveyServiceTest {
 	}
 
 	@Test
-	public void testSurveysDeletion(){
+	public void testDeleteAllSurveys(){
 		try {
 
 
@@ -283,7 +307,7 @@ public class SurveyServiceTest {
 	}
 
 	@Test
-	public void testExistingSurveyDeletion(){
+	public void testDeleteExistingSurvey(){
 		try {
 
 			// first add a couple of surveys
@@ -318,7 +342,7 @@ public class SurveyServiceTest {
 			fail("Detected invalid survey URL! " + e.getMessage());
 		}
 	}
-
+	
 	@Test
 	public void testDeleteNonExistingSurvey(){
 		// check if deletion of non-existing surveys works.
@@ -326,12 +350,47 @@ public class SurveyServiceTest {
 		assertEquals(404,delete.getHttpCode());
 	}
 
-	//@Test
-	public void keepOpen(){
-		try {
-			Thread.sleep(20000000);
-		} catch (InterruptedException e) {
+	@Test
+	public void testUpdateExistingSurvey(){
+		// first add a new survey
+		ClientResponse r = c.sendRequest("POST", "mobsos/surveys",generateSurveyJSON().toJSONString());
+		assertTrue(r.getResponse()!=null);
+		assertTrue(r.getResponse().length()>0);
+		try{
+			JSONObject o = (JSONObject) JSONValue.parseWithException(r.getResponse().trim());
+			assertTrue(o.keySet().contains("url"));
+
+			String fullurl = (String) o.get("url");
+
+			// check if first survey URL is a valid URL, then extract path
+			URL u = new URL(fullurl);
+			String pathonly = u.getPath();
+
+			// use path to get the survey
+			ClientResponse result=c.sendRequest("GET", u.getPath(),"");
+			assertEquals(200,result.getHttpCode()); // survey should exist
+			JSONObject survey = (JSONObject) JSONValue.parse(result.getResponse());
+
+			// change some fields in survey
+			survey.put("name","Cuntsucking Asslicker Survey");
+			survey.put("description", "This survey is for all those who like to suck cunt, lick ass and to do Cleveland steaming.");
+
+			// then call service to update existing survey
+			ClientResponse updateresult=c.sendRequest("POST", u.getPath(),survey.toJSONString());
+			assertEquals(200, updateresult.getHttpCode());
+
+			ClientResponse updated=c.sendRequest("GET", u.getPath(),"");
+			assertEquals(200,updated.getHttpCode()); // survey should exist
+			JSONObject updatedSurvey = (JSONObject) JSONValue.parse(updated.getResponse());
+			
+			assertEquals(survey,updatedSurvey);
+
+		} catch (ParseException e) {
 			e.printStackTrace();
+			fail("Service returned invalid JSON! " + e.getMessage());
+		} catch (MalformedURLException e){
+			e.printStackTrace();
+			fail("Service returned malformed URL!");
 		}
 	}
 
