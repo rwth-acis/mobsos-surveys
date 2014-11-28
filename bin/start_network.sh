bb6ac6c836953ed50a948322dc207afaef4300c7
Dominik Renzel <dominik.renzel@gmail.com>
2014-11-28 13:03:28 +0100

    Adapted bash start script to new las2peer version

--- bin/start_network.sh
+++ bin/start_network.sh
@@ -5,4 +5,4 @@ cd ${SCRIPTDIR}/../
 BASE=${PWD}
 export CLASSPATH="${PWD}/lib/*"
 
-java -cp "${CLASSPATH}" i5.las2peer.tools.L2pNodeLauncher -s 9001 - "uploadStartupDirectory" "startService('i5.las2peer.services.mobsos.SurveyService','mobsosrules')" "startConnector('i5.las2peer.webConnector.WebConnector')" 
+java -cp "${CLASSPATH}" i5.las2peer.tools.L2pNodeLauncher -w -p 9001 - "uploadStartupDirectory('etc/startup')" "startService('i5.las2peer.services.mobsos.SurveyService','mobsosrules')" "startWebConnector" 
