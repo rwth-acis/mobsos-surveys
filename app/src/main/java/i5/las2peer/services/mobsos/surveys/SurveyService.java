5d4f6a30eaf887ba09d3281e1f4d8e9684e11b6d
Theresa Täuber <theresa.taeuber@rwth-aachen.de>
2022-01-24 15:44:04 +0100

    fix for incorrect order of questions in json object

--- app/src/main/java/i5/las2peer/services/mobsos/surveys/SurveyService.java
+++ app/src/main/java/i5/las2peer/services/mobsos/surveys/SurveyService.java
@@ -4862,6 +4862,7 @@ public class SurveyService extends RESTService {
 		private JSONObject extractQuestionInformation(Document questionnaireDocument) {
 
 			JSONObject questions = new JSONObject();
+			Map orderedQuestions = new LinkedHashMap();
 
 			NodeList nodeList = questionnaireDocument.getElementsByTagNameNS(MOBSOS_QUESTIONNAIRE_NS, "Page");
 			for (int i = 0; i < nodeList.getLength(); i++) {
@@ -4895,10 +4896,12 @@ public class SurveyService extends RESTService {
 						Node instructionNode = e.getElementsByTagName("qu:Instructions").item(0);
 						question.put("instructions",instructionNode.getTextContent());
 
-						questions.put(e.getAttribute("qid"), question);
+						orderedQuestions.put(e.getAttribute("qid"), question);
 					}
 				}
 			}
+
+			questions = (JSONObject) orderedQuestions;
 			return questions;
 		}
 
